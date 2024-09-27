ae532c41f1ff01c7a002fb1c02a31c8e2c659ee8
Daniel R. Weilandt <daniel.weilandt@princeton.edu>
2024-09-27 17:02:59 -0400

    ENH: Final model with pH and psi control

--- mechanistic_model/parameter_sampling.py
+++ mechanistic_model/parameter_sampling.py
@@ -140,43 +140,43 @@ if __name__ == '__main__':
     # Arround 1 uM
     kmodel.reactions.LDH_L.parameters.k_inhibition_IM_pmtcoa_c_LDH_L.bounds = (0.8e-3 , 1.2e-3)
 
+    # # BDHm 
+    # # https://www.brenda-enzymes.org/all_enzymes.php?ecno=1.1.1.30&table=KM_Value#TAB
+    # kmodel.reactions.BDHm.parameters.km_substrate1.bounds = (0.05, 0.07) # NAD
+    # kmodel.reactions.BDHm.parameters.km_substrate2.bounds = (1.0, 5.0) # 3BHb -> GUESS
+    # kmodel.reactions.BDHm.parameters.km_product1.bounds = (0.02, 0.03) # NADH
+    # kmodel.reactions.BDHm.parameters.km_product2.bounds = (0.25, 0.35) # AcAc
 
-    # BDHm 
-    # https://www.brenda-enzymes.org/all_enzymes.php?ecno=1.1.1.30&table=KM_Value#TAB
-    kmodel.reactions.BDHm.parameters.km_substrate1.bounds = (0.05, 0.07) # NAD
-    kmodel.reactions.BDHm.parameters.km_substrate2.bounds = (0.3, 0.7) # 3BHb -> GUESS
-    kmodel.reactions.BDHm.parameters.km_product1.bounds = (0.02, 0.03) # NADH
-    kmodel.reactions.BDHm.parameters.km_product2.bounds = (0.25, 0.35) # AcAc
+    # # OCOAT1m/ SCOT1
+    # # https://www.brenda-enzymes.org/enzyme.php?ecno=2.8.3.5
+    # kmodel.reactions.OCOAT1m.parameters.km_substrate1.bounds = (0.1, 0.3) # SucCoa
+    # kmodel.reactions.OCOAT1m.parameters.km_substrate2.bounds = (0.05, 0.1) # ACAC 0.07
+    # kmodel.reactions.OCOAT1m.parameters.km_product1.bounds = (0.02, 0.03) # Succ
+    # kmodel.reactions.OCOAT1m.parameters.km_product2.bounds = (0.04, 0.06) # AcAcCoa
 
-    # OCOAT1m/ SCOT1
-    # https://www.brenda-enzymes.org/enzyme.php?ecno=2.8.3.5
-    kmodel.reactions.OCOAT1m.parameters.km_substrate1.bounds = (0.1, 0.3) # SucCoa
-    kmodel.reactions.OCOAT1m.parameters.km_substrate2.bounds = (0.05, 0.1) # ACAC 0.07
-    kmodel.reactions.OCOAT1m.parameters.km_product1.bounds = (0.02, 0.03) # Succ
-    kmodel.reactions.OCOAT1m.parameters.km_product2.bounds = (0.04, 0.06) # AcAcCoa
+    # #Acetyl CoA C acetyltransferase mitochondrial
+    # # https://www.brenda-enzymes.org/enzyme.php?ecno=2.3.1.9#KM%20VALUE%20[mM]
+    # kmodel.reactions.ACACT1rm.parameters.km_substrate1.bounds = (0.03, 0.031) # AcCoa
+    # kmodel.reactions.ACACT1rm.parameters.km_substrate2.bounds = (0.03, 0.031) # AcCoa
+    # kmodel.reactions.ACACT1rm.parameters.km_product1.bounds = (0.01, 0.03) # CoA
+    # kmodel.reactions.ACACT1rm.parameters.km_product2.bounds = (4e-3, 5e-3) # AcAcCoa
 
-    #Acetyl CoA C acetyltransferase mitochondrial
-    # https://www.brenda-enzymes.org/enzyme.php?ecno=2.3.1.9#KM%20VALUE%20[mM]
-    kmodel.reactions.ACACT1rm.parameters.km_substrate1.bounds = (0.03, 0.031) # AcCoa
-    kmodel.reactions.ACACT1rm.parameters.km_substrate2.bounds = (0.03, 0.031) # AcCoa
-    kmodel.reactions.ACACT1rm.parameters.km_product1.bounds = (0.01, 0.03) # CoA
-    kmodel.reactions.ACACT1rm.parameters.km_product2.bounds = (4e-3, 5e-3) # AcAcCoa
+    # # # G3PD1 - Glycerol 3-phosphate dehydrogenase (NAD+)
+    # # # https://www.brenda-enzymes.org/enzyme.php?ecno=1.1.1.8#KM%20VALUE%20[mM]
+    # kmodel.reactions.G3PD1.parameters.km_product1.bounds = (0.002, 0.010) # NADH
+    # kmodel.reactions.G3PD1.parameters.km_product2.bounds = (0.02, 0.03) # DAHP
+    # kmodel.reactions.G3PD1.parameters.km_substrate1.bounds = (0.01, 0.04) # NAD+
+    # kmodel.reactions.G3PD1.parameters.km_substrate2.bounds = (0.5,1.0) # Glycerol-3P
 
-    # # G3PD1 - Glycerol 3-phosphate dehydrogenase (NAD+)
-    # # https://www.brenda-enzymes.org/enzyme.php?ecno=1.1.1.8#KM%20VALUE%20[mM]
-    kmodel.reactions.G3PD1.parameters.km_product1.bounds = (0.002, 0.010) # NADH
-    kmodel.reactions.G3PD1.parameters.km_product2.bounds = (0.02, 0.03) # DAHP
-    kmodel.reactions.G3PD1.parameters.km_substrate1.bounds = (0.01, 0.04) # NAD+
-    kmodel.reactions.G3PD1.parameters.km_substrate2.bounds = (0.5,1.0) # Glycerol-3P
-
-    # r0205 - GPD2 - Glycerol-3-phosphate dehydrogenase (Quinone)
-    # Brenda 
-    kmodel.reactions.r0205.parameters.km_substrate2.bounds = (6, 10) # Glycerol-3P
+    # # r0205 - GPD2 - Glycerol-3-phosphate dehydrogenase (Quinone)
+    # # Brenda 
+    # kmodel.reactions.r0205.parameters.km_substrate2.bounds = (6, 10) # Glycerol-3P
 
     # TPI - le
     # https://www.brenda-enzymes.org/enzyme.php?ecno=5.3.1.1#KM%20VALUE%20[mM]
     kmodel.reactions.TPI.parameters.km_substrate.bounds = (0.5, 1.5) #DHAP
     kmodel.reactions.TPI.parameters.km_product.bounds = (0.25, 1.0) # GAP
+
 
     # NOTE DW -> TRANSPORTERS SHOULD HAVE SAME KM for substrate and product pairs
     
@@ -210,6 +210,13 @@ if __name__ == '__main__':
         kmodel.reactions.ASPGLUm.parameters.km_substrate2.bounds = (asp_L_c*4.9, asp_L_c*5)
         kmodel.reactions.ASPGLUm.parameters.km_product1.bounds = (glu_L_c*4.9, glu_L_c*5)
         kmodel.reactions.ASPGLUm.parameters.km_product2.bounds = (asp_L_c*4.9, asp_L_c*5)
+
+        # BDH and OCOAT1m unsaturated in their ketones effects
+        # AcAc and 3HB
+        acac_m = concentrations['acac_m']
+        bhb_m = concentrations['bhb_m']
+        kmodel.reactions.BDHm.parameters.km_substrate2.bounds = (bhb_m*5, bhb_m*10)
+        kmodel.reactions.OCOAT1m.parameters.km_substrate2.bounds = (acac_m*5, acac_m*10)
 
         # Fetch equilibrium constants
         load_equilibrium_constants(sample, tmodel, kmodel,
